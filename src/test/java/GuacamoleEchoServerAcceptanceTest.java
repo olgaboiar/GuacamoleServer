61431bd606d9de32e69656ec6729c7785bbb5e98
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-01-09 15:28:59 -0500

    trying junit

---
+++ src/test/java/GuacamoleEchoServerAcceptanceTest.java
@@ -0,0 +1,15 @@
+package test.java;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class GuacamoleEchoServerAcceptanceTest {
+//    @BeforeEach
+//    start server
+//    @AfterEach
+//    stop server
+    @Test
+//    run test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
