d9a76ed46fbe7457d91c706f28360f7f4bc3e8c4
Olga Boiaryntseva <olgaboiar@gmail.com>
2019-01-14 17:01:59 -0500

    added cucucmber runner class

---
+++ src/test/java/gradle/cucumber/Steps.java
@@ -0,0 +1,11 @@
+package gradle.cucumber;
+
+import cucumber.api.java.en.Given;
+
+public class Steps {
+    @Given("Server process is started")
+    public void the_server_is_started() throws Throwable {
+
+    }
+
+}
